33e31a9ef6a1a365467499fabcab3ed9cddf6317
xihuyu20000 <377486624@qq.com>
2020-09-18 10:52:18 +0800

    App: 定时刷新token

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -6,7 +6,21 @@
 
 <script>
 export default {
-  name: "app"
+  name: "app",
+  methods: {
+    fetch() {
+      setInterval(async () => {
+        const { data: resp } = await this.$http.get("/token");
+        if (resp.status == 200) {
+          sessionStorage.setItem("token", resp.token);
+        }
+        console.log("定时5秒", resp);
+      }, 5000 * 20 * 30);
+    },
+  },
+  created() {
+    this.fetch();
+  },
 };
 </script>
 
