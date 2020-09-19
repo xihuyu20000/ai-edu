efe4c6170d4059ee34dbb3ca94703938813b245d
xihuyu20000 <377486624@qq.com>
2020-09-19 12:55:40 +0800

    增加进度条

--- admin/src/plugins/axios.js
+++ admin/src/plugins/axios.js
@@ -3,8 +3,12 @@ import axios from "axios";
 axios.defaults.baseURL = "http://localhost:33333/api";
 axios.defaults.timeout = 30000;
 
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
 axios.interceptors.request.use(
   (config) => {
+    NProgress.start();
     console.log("请求", config);
     config.headers["Authorization"] = sessionStorage.getItem("token");
     return config;
@@ -15,4 +19,8 @@ axios.interceptors.request.use(
   }
 );
 
+axios.interceptors.response.use((config) => {
+  NProgress.done();
+  return config;
+});
 Vue.prototype.$http = axios;
