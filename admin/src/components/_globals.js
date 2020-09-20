423aaf83e95c4f87de5596886f1e06e2e804cac2
xihuyu20000 <377486624@qq.com>
2020-09-20 08:30:59 +0800

    components/_globals: 真正实现全局注册插件

--- admin/src/components/_globals.js
+++ admin/src/components/_globals.js
@@ -1,5 +1,8 @@
 import Vue from "vue";
 // 这里的注册名，必须是小写，并且在调用的时候，也要小写
-Vue.component("create-form", "@/components/CreateForm");
-Vue.component("edit-form", "@/components/EditForm");
-Vue.component("common-table", "@/components/CommonTable");
+import CommonTable from "./CommonTable";
+import CreateForm from "./CreateForm";
+import EditForm from "./EditForm";
+Vue.component("common-table", CommonTable);
+Vue.component("edit-form", CreateForm);
+Vue.component("create-form", EditForm);
