418ddc176078024392fb495d730da02cd200ad0b
xihuyu20000 <377486624@qq.com>
2020-09-26 01:42:18 +0800

    封装request爬虫工具

---
+++ spider/index.js
@@ -0,0 +1,34 @@
+const { get, post, render } = require("./downloader");
+const cheerio = require("cheerio");
+const log = console.log;
+
+// 使用get方法
+const doGet = () =>
+  get("http://www.ruankao.org.cn/index/work").then(($) => {
+    $("ul li").each((i, v) =>
+      log(i, $(v).find("label").text(), $(v).find("a").text())
+    );
+  });
+doGet();
+
+const doPost = () => {
+  for (let i = 1; i < 7; i++) {
+    post("http://www.ruankao.org.cn/index/work", {
+      method: "post",
+      formData: { page: i, page_size: 10 },
+    }).then(($) => {
+      $("ul li").each((i, v) =>
+        log(i, $(v).find("label").text(), $(v).find("a").text())
+      );
+    });
+  }
+};
+// doPost();
+
+const doRender = () =>
+  render("https://3g.163.com/touch/news/").then(($) => {
+    $(".title").each((i, v) => log(i, $(v).text()));
+  });
+// doRender();
+
+// 研究一下 https://github.com/bda-research/node-crawler
