fa8bf49d50ca5fc938a365db35b2569aef8c062e
xihuyu20000 <377486624@qq.com>
2020-10-12 22:22:44 +0800

    eel结合vuejs

---
+++ easyexam/frontend/vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  publicPath: "", // 部署应用时的基本URL
+  outputDir: "../web", // build时构建文件的目录
+  assetsDir: "", // build时放置生成的镜头资源（相对于outputDir）
+  indexPath: "index.html", // 指定生成的index.html的输出路径（相对于outputDir）
+};
---
+++ easyexam/start.bat
@@ -0,0 +1,3 @@
+rmdir /s/q build
+rmdir /s/q dist
+G:\workspace\workspace-js\ai-edu\easyexam\env\Scripts\python.exe -m eel  index.py  web  -i icon.ico  -n easyexam -D --clean  --onefile  --hidden-import eel
